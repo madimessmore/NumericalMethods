ec06fdfd846d2a6da4f0a3c765bcd406d630f1c8
Madi Messmore <55211235+madimessmore@users.noreply.github.com>
2021-03-12 14:24:57 -0500

    Update partialDeriv.py

--- partialDeriv.py
+++ partialDeriv.py
@@ -3,26 +3,24 @@ import numpy as np
 
 # creating our symbolic variables
 x = Symbol('x')
-
 y = Symbol('y')
-
 z = Symbol('z')
 
 # Initial Guess Vector
-x0 = 2
-y0 = 2
+x0 = 1
+y0 = 1
 z0 = 2
 # a0 = 2
 # b0 = 2
 # c0 = 2
-guess = np.array([2, 2, 2])
+guess = np.array([[1], [1], [1]])
 # sol1 = np.array([])
 # sol2 = np.array([])
 # sol3 = np.array([])
 
 # our functions in the non-linear system
-function1 = x**2 + y**3
-function2 = x**2 + y**3 + z**2
+function1 = x**2 - y
+function2 = 2*x + y**3
 function3 = 2*x**3 + y**2 + 3*z**3
 
 solutionVect = np.array([[function1.evalf(subs={x: x0, y: y0, z: z0})],
@@ -84,78 +82,64 @@ JacobMatrixSymbolic = np.array([[partialDerivXf1, partialDerivYf1, partialDerivZ
 print(JacobMatrixSymbolic)
 print("\n")
 
-tol = 0.000001
-overtol = 10000000
-
-for JacobianMatrix in range (10):
-    # Evaluating each partial of each function at the new x, y, and z values
-    print("Each Partial of Function 1 evaluated at the new values:")
-    print(partialDerivXf1.evalf(subs={x: x0, y: y0, z: z0}))
-    comp1 = partialDerivXf1.evalf(subs={x: x0, y: y0, z: z0})
-    print(partialDerivYf1.evalf(subs={x: x0, y: y0, z: z0}))
-    comp2 = partialDerivYf1.evalf(subs={x: x0, y: y0, z: z0})
-    print(partialDerivZf1.evalf(subs={x: x0, y: y0, z: z0}))
-    comp3 = partialDerivZf1.evalf(subs={x: x0, y: y0, z: z0})
-    print("\nEach partial of function 2 evaluated at the new values:")
-    print(partialDerivXf2.evalf(subs={x: x0, y: y0, z: z0}))
-    comp4 = partialDerivXf2.evalf(subs={x: x0, y: y0, z: z0})
-    print(partialDerivYf2.evalf(subs={x: x0, y: y0, z: z0}))
-    comp5 = partialDerivYf2.evalf(subs={x: x0, y: y0, z: z0})
-    print(partialDerivZf2.evalf(subs={x: x0, y: y0, z: z0}))
-    comp6 = partialDerivZf2.evalf(subs={x: x0, y: y0, z: z0})
-    print("\nEach partial of function 3 evaluated at the new values:")
-    print(partialDerivXf3.evalf(subs={x: x0, y: y0, z: z0}))
-    comp7 = partialDerivXf3.evalf(subs={x: x0, y: y0, z: z0})
-    print(partialDerivYf3.evalf(subs={x: x0, y: y0, z: z0}))
-    comp8 = partialDerivYf3.evalf(subs={x: x0, y: y0, z: z0})
-    print(partialDerivZf3.evalf(subs={x: x0, y: y0, z: z0}))
-    comp9 = partialDerivZf3.evalf(subs={x: x0, y: y0, z: z0})
+# Evaluating each partial of each function at the new x, y, and z values
+print("Each Partial of Function 1 evaluated at the new values:")
+print(partialDerivXf1.evalf(subs={x: x0, y: y0, z: z0}))
+comp1 = partialDerivXf1.evalf(subs={x: x0, y: y0, z: z0})
+print(partialDerivYf1.evalf(subs={x: x0, y: y0, z: z0}))
+comp2 = partialDerivYf1.evalf(subs={x: x0, y: y0, z: z0})
+print(partialDerivZf1.evalf(subs={x: x0, y: y0, z: z0}))
+comp3 = partialDerivZf1.evalf(subs={x: x0, y: y0, z: z0})
+print("\nEach partial of function 2 evaluated at the new values:")
+print(partialDerivXf2.evalf(subs={x: x0, y: y0, z: z0}))
+comp4 = partialDerivXf2.evalf(subs={x: x0, y: y0, z: z0})
+print(partialDerivYf2.evalf(subs={x: x0, y: y0, z: z0}))
+comp5 = partialDerivYf2.evalf(subs={x: x0, y: y0, z: z0})
+print(partialDerivZf2.evalf(subs={x: x0, y: y0, z: z0}))
+comp6 = partialDerivZf2.evalf(subs={x: x0, y: y0, z: z0})
+print("\nEach partial of function 3 evaluated at the new values:")
+print(partialDerivXf3.evalf(subs={x: x0, y: y0, z: z0}))
+comp7 = partialDerivXf3.evalf(subs={x: x0, y: y0, z: z0})
+print(partialDerivYf3.evalf(subs={x: x0, y: y0, z: z0}))
+comp8 = partialDerivYf3.evalf(subs={x: x0, y: y0, z: z0})
+print(partialDerivZf3.evalf(subs={x: x0, y: y0, z: z0}))
+comp9 = partialDerivZf3.evalf(subs={x: x0, y: y0, z: z0})
 
 
-    JacobianMatrix = np.array([[comp1, comp2, comp3],
-                         [comp4, comp5, comp6],
-                         [comp7, comp8, comp9]], dtype='double')
+JacobianMatrix = np.array([[comp1, comp2, comp3],
+                     [comp4, comp5, comp6],
+                     [comp7, comp8, comp9]], dtype='double')
+
+print("\nThe Evaluation of the Jacobian Matrix at this iteration is: ")
+print(JacobianMatrix)
+print("\n")
 
 
-    InvJacobianMatrix = np.linalg.inv(JacobianMatrix)
+InvJacobianMatrix = np.linalg.inv(JacobianMatrix)
 
-    Part = InvJacobianMatrix.dot(solutionVect)
+Part = InvJacobianMatrix.dot(solutionVect)
 
-    NewSolutionVect = solutionVect - Part
+NewSolutionVect = guess - Part
 
-    convergence = abs(NewSolutionVect-solutionVect)
+convergence = abs(NewSolutionVect-solutionVect)
 
-    print("\nThe Solution Vector at x in beginning of iteration:")
-    print(solutionVect)
+print("\nThe Solution Vector at x in beginning of iteration:")
+print(solutionVect)
 
-    print("\nThe Inverse of the Jacobian at entered solution vector:")
-    print(InvJacobianMatrix)
+print("\nThe Inverse of the Jacobian at entered solution vector:")
+print(InvJacobianMatrix)
 
-    print("\nThe product of the solution vector and the inverse Jacobian:")
-    print(Part)
+print("\nThe product of the solution vector and the inverse Jacobian:")
+print(Part)
 
-    print("\nOur New X for the next iteration:")
-    print(NewSolutionVect)
-
-    '''
-    print("\nThe Residual of this Iteration is:")
-    print(residual)
-    '''
-
-    print("\nThe Iterative Convergence of this Iteration is:")
-    print(convergence)
-
-    print("Note: The Residual is the absolute value of the largest component of the solution vector")
-
-    if convergence > overtol:
-        print("This bitch Diverging. YEET")
-        converge = false
-    else:
-        converge = true
+print("\nOur New X for the next iteration:")
+print(NewSolutionVect)
 
 
+print("\nThe Iterative Convergence of this Iteration is:")
+print(convergence)
 
-solutionVect = guess
+print("Note: The Residual is the absolute value of the largest component of the solution vector")
 
 #print("The inverse of the Jacobian Matrix is:")
 # print(np.linalg.inv(JacobianMatrix))
